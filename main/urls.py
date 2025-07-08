0dcc09fa4d4bd94a64f431f9c4453cbaf3155e12
mavlidin-creator <mavlidinasanuulu@gmail.com>
2025-07-08 16:15:19 +0300

    HomeWork_1

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('product.urls')),
+    path('api/v1/', include('product.urls')),
 ]
